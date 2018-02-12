16b4dda05f29a9cecae3970efb3a964180833669
Z4Knight <z4knight@163.com>
2018-02-12 16:49:19 +0800

    修改启动类Mapper扫描路径

--- src/main/java/com/z4knight/bugmanagement/Application.java
+++ src/main/java/com/z4knight/bugmanagement/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.z4knight.bugmanagement.dataobject")
+@MapperScan(basePackages = "com.z4knight.bugmanagement.repository")
 public class Application {
 
 	public static void main(String[] args) {
